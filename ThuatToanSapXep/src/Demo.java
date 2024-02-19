db467ed402fb12ea0dbd2376b764bb82869563e1
vulinh1704 <truongdanglinh1704@gmail.com>
2024-02-19 09:29:31 +0700

    sort

---
+++ ThuatToanSapXep/src/Demo.java
@@ -0,0 +1,55 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Demo {
+    public static void main(String[] args) {
+//        bubbleSort();
+//        selectionSort();
+        insertSort();
+    }
+
+    public static void bubbleSort() {
+        int[] arr = new int[]{33, 21, 4, 5, 7, 9};
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr.length - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+        System.out.println(Arrays.toString(arr));
+    }
+
+    public static void selectionSort() {
+        int[] arr = new int[]{33, 21, 4, 5, 7, 9};
+        for (int i = 0; i < arr.length; i++) {
+            int minIndex = i;
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[j] < arr[minIndex]) {
+                    minIndex = j;
+                }
+            }
+            int temp = arr[i];
+            arr[i] = arr[minIndex];
+            arr[minIndex] = temp;
+        }
+        System.out.println(Arrays.toString(arr));
+    }
+
+    public static void insertSort() {
+        int[] arr = new int[]{33, 21, 4, 5, 7, 9};
+        for (int i = 1; i < arr.length; i++) {
+            for (int j = i; j > 0; j--) {
+                if (arr[j] < arr[j - 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j - 1];
+                    arr[j - 1] = temp;
+                }
+            }
+        }
+        System.out.println(Arrays.toString(arr));
+    }
+}
